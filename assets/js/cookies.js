30cac60cc82744a91b32634c28dd3acaccbc3bd8
Silver Kuusik <silver.kuusik@gmail.com>
2019-02-24 20:53:48 +0100

    Update cookies.js

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,6 +1,11 @@
 window.addEventListener("load", function() {
     window.cookieconsent.initialise({
         "type": "info",
-        "position": "bottom-right"
+        "position": "bottom-right",
+        "palette": {
+            "popup": {
+                "background": "#242943ff"
+            }
+        }
     })
 });
